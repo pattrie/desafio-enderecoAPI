f6d6f5e8157951887bb2874bcce2f4c1b2bede51
Patrícia <patri.b.morais@gmail.com>
2021-07-04 22:30:50 -0300

    Ajustes - remoção de informações do MongoDB

--- src/main/java/com/br/desafio/endereco/EnderecoClienteMapper.java
+++
@@ -1,9 +0,0 @@
-package com.br.desafio.endereco;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface EnderecoClienteMapper {
-
-    EnderecoClienteDto toEnderecoDto(EnderecoClienteEntity endereco);
-    EnderecoClienteEntity toEndereco(EnderecoClienteDto endereco);
-}
--- src/main/java/com/br/desafio/endereco/EnderecoClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.desafio.endereco;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EnderecoClienteRepository extends MongoRepository<EnderecoClienteEntity, String> {
-
-    EnderecoClienteDto findByCep(String cep);
-}
